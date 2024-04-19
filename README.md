4b6b08327bb3d398791f9ce969b9e25fada2b29b
naj <najnesnaj@yahoo.com>
2024-04-19 20:13:55 +0200

    add dha hack and stm32-ECM simulation

--- README.md
+++ README.md
@@ -10,5 +10,9 @@ The ultimate goal is an EV conversion.
 
 I modified the OpenMoose program, to send messages over the canbus using the Volvo DICE cable. (this already works!) Unfortunately I'm a lousy programmer and not really at ease in the Microsoft world, so if anybody can help out, send me a mail. I'm working on sniffing canbustraffic using the J2534 DLL. Should work, I guess, and it would be really useful since no extra hardware would be needed.
 
-You can find a modified stm32 openlib3 program, which ultimate goal is to simulate an ECU. But this is of course work in progress.
+You can find a modified stm32 openlib3 program, which ultimate goal is to simulate an ECU. But this is of course work in progress. The program responds on diagnostic code (B9F0), which delivers the part number, and software number.
 
+(as a workaround for the openmoose mod) 
+In the manual I put a download link for the DHA (diagnostic host application) software. This software works with the DICE cable, and allows to send diagnostic messages and (!) receive the response.
+It can open a database in DDB format, which is text that can be edited. I modified/simplified the existing database so it works with my Volvo (file volvoV50.DDB). Upon (Read Data Block By Offset/ECU identification) it interacts with my stm32 microcontroller, which fools the diagnostic software into dealing with a Volvo. (Volvos have never been cheaper ;))
+~                         
--- stm32f103c6-canbus/src/main.cpp
+++ stm32f103c6-canbus/src/main.cpp
@@ -40,50 +40,84 @@ void init() {
 	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 
 }
+		CAN::Frame frame, frame1, frame2;
 
 void loop() {
-	// Loop over any characters pending in the UART Rx buffer,
-	// and send each one over the CAN bus as a single message.
-	char c;
-	c='e';
-	//do not wait for user input, send message at 500kbs to simulate
-	while (Uart::get(&c)) {
-		printf(" smile!\n");
-		gpio_set(GPIOC, GPIO13);
-		Uart::put(c);
-				for (int i = 0; i < 6000; i++) { /* Wait a bit. */
-					__asm__("nop");
-				}
 
-		CAN::Frame frame;
-		frame.id = 0x00222222; //pretend this is the CEM module
+	// Loop over any CAN frames pending in the CAN buffer, and print out
+	// the ID of the message and all the data bytes.
+	CAN::Frame framereceived;
+	while (CAN::pop(framereceived)) {
+		gpio_set(GPIOC, GPIO13);
+		// Do something with the received frame
+		printf("Rx ID: %u Data: ", framereceived.id);
+		printf("Hexadecimal representation: 0x%X\n", framereceived.id);
+		for (int i = 0; i < framereceived.len; i++) {
+			printf("%02x:", framereceived.data[i]);
+		}
+                if (framereceived.data[1]==80){ //80 is 0x50 hex
+                printf("ECM request\n");
+
+//First byte in reply is a technical field, like rolling counter, start (0x80)/end(0x40 flag of the multipart message, etc. The
+//P/N is: 08 68 85 13 == 8688513
+
+for (int i = 0; i < 6000; i++) { /* Wait a bit. */
+                                        __asm__("nop");
+                                }
+
+		frame.id = 0x01000003; //pretend this is the CEM module
 		frame.extended_id = true; //volvo uses 29bit
 		frame.rtr = false;
 		frame.len = 8;
 		// Populate the payload bytes with accelerator pedal 0x3F92
-		frame.data[0] = 0xCE; //length = 5
+		frame.data[0] = 0x8F; //
 		frame.data[1] = 0x50; //CEM module
-		frame.data[2] = 0xE6; //reply to A6
-		frame.data[3] = 0x3F; //accel pedal
-		frame.data[4] = 0x92;
-		frame.data[5] = 0x24; //value
-		frame.data[6] = 0x01; //number of messages
-		frame.data[7] = 0x00;
+		frame.data[2] = 0xF9; //
+		frame.data[3] = 0xF0; //
+		frame.data[4] = 0x00;
+		frame.data[5] = 0x08; //
+		frame.data[6] = 0x68; //
+		frame.data[7] = 0x85;
 		CAN::transmit(frame);
-		gpio_clear(GPIOC, GPIO13);
-	}
 
-	// Loop over any CAN frames pending in the CAN buffer, and print out
-	// the ID of the message and all the data bytes.
-	CAN::Frame frame;
-	while (CAN::pop(frame)) {
-		gpio_set(GPIOC, GPIO13);
-		// Do something with the received frame
-		printf("Rx ID: %u Data: ", frame.id);
-		printf("Hexadecimal representation: 0x%X\n", frame.id);
-		for (int i = 0; i < frame.len; i++) {
-			printf("%02x:", frame.data[i]);
-		}
+for (int i = 0; i < 6000; i++) { /* Wait a bit. */
+                                        __asm__("nop");
+                                }
+
+		frame1.id = 0x01000003; //pretend this is the CEM module
+		frame1.extended_id = true; //volvo uses 29bit
+		frame1.rtr = false;
+		frame1.len = 8;
+		// Populate the payload bytes with accelerator pedal 0x3F92
+		frame1.data[0] = 0x09; //
+		frame1.data[1] = 0x13; //CEM module
+		frame1.data[2] = 0x20; //
+		frame1.data[3] = 0x20; //
+		frame1.data[4] = 0x41;
+		frame1.data[5] = 0x30; //
+		frame1.data[6] = 0x65; //
+		frame1.data[7] = 0x70;
+		CAN::transmit(frame1);
+
+for (int i = 0; i < 6000; i++) { /* Wait a bit. */
+                                        __asm__("nop");
+                                }
+
+		frame2.id = 0x01000003; //pretend this is the CEM module
+		frame2.extended_id = true; //volvo uses 29bit
+		frame2.rtr = false;
+		frame2.len = 8;
+		// Populate the payload bytes with accelerator pedal 0x3F92
+		frame2.data[0] = 0x4C; 
+		frame2.data[1] = 0x24; 
+		frame2.data[2] = 0x20; 
+		frame2.data[3] = 0x20; 
+		frame2.data[4] = 0x41;
+		frame2.data[5] = 0x00; 
+		frame2.data[6] = 0x00;
+		frame2.data[7] = 0x00;
+		CAN::transmit(frame2);
+                }
 		printf("\n");
 		gpio_clear(GPIOC, GPIO13);
 	}
